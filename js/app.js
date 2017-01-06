4ce0c2384d65a1daddcb2d1c2ad9ca2e8d4593a1
Dana Ottaviani <dana.ottaviani@gmail.com>
2017-01-06 17:43:09 -0500

    added timer interval to slideshow

--- js/app.js
+++ js/app.js
@@ -1,38 +1,50 @@
 let leftButton = document.getElementById('left');
 let rightButton = document.getElementById('right');
 let imagePlace = document.getElementById('img-place');
+let image = document.getElementById('flower');
 
 let images = ['yellow', 'blue', 'purple', 'rainbow'];
-let image = '';
 let index = 0;
 
-// function resize(img){
-// 	if (img.width > img.height){
-// 		img.style.width = 800px;
-// 		img.height = 
-
-// 	}
-
-// }
-
-leftButton.addEventListener('click', function(){
-	$("#img-place").fadeOut(1000, function(){
-		index--;
-		if(index < 0){
-			index = images.length - 1;
-		}
-		$(this).html('<img id="flower" class="img-responsive" src="images/' + images[index] + '-rose.jpg">');
-	}); 
-	$("#img-place").fadeIn(2000);
-});
-
-rightButton.addEventListener('click', function(){ 
+function moveRight(){
 	$("#img-place").fadeOut(1000, function(){
 		index++;
 		if(index == images.length){
 			index = 0;
 		}
-		$(this).html('<img id="flower" class="img-responsive" src="images/' + images[index] + '-rose.jpg">');
+		image.setAttribute('src', "images/" + images[index] + "-rose.jpg");
 	}); 
 	$("#img-place").fadeIn(2000);
-});
+}
+
+function moveLeft(){
+	$("#img-place").fadeOut(1000, function(){
+		index--;
+		if(index < 0){
+			index = images.length - 1;
+		}
+		image.setAttribute('src', "images/" + images[index] + "-rose.jpg");
+	}); 
+	$("#img-place").fadeIn(2000);
+};
+
+let timer;
+
+function startTimer(){
+	timer = window.setInterval(moveRight, 5000);
+}
+
+// function stopTimer(){
+// 	window.clearInterval(timer);
+
+// 	leftButton.removeEventListener('click', stopTimer);
+// 	rightButton.removeEventListener('click', stopTimer);
+
+
+// 	//document.addEventListener('click', startTimer);
+// }
+
+window.addEventListener('load', startTimer);
+
+leftButton.addEventListener('click', moveLeft);
+rightButton.addEventListener('click', moveRight);
