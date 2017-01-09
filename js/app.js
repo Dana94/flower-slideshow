f87092ec943b68efe5ad9541635dd14890a54e25
Dana Ottaviani <dana.ottaviani@gmail.com>
2017-01-09 17:50:43 -0500

    still resizing the images

--- js/app.js
+++ js/app.js
@@ -3,17 +3,22 @@ let rightButton = document.getElementById('right');
 let imagePlace = document.getElementById('img-place');
 let image = document.getElementsByClassName('flower');
 
-let images = ['yellow', 'blue', 'purple', 'rainbow'];
+let images = ['blue', 'yellow', 'purple', 'rainbow'];
 let index = 0;
 
+
 // function resize(){
-// 	if (image[0].getAttribute('src') == "images/yellow-rose.jpg"){
-		
-// 	}
-// 	else{
-// 		image[0].css('width', 100%).css('height', 0);
-// 	}
-// }
+// 	if (image[0].height > image[0].width){
+// 			console.log('y');
+// 			$('#img-place').css('margin-top', -30);
+// 			$("#img-place").css('height', 500);
+// 		}
+// 		else{
+// 			console.log('n');
+// 			$('#img-place').css('padding-top', 30);
+// 			$("#img-place").css('height', 450);
+// 		}
+//  }
 
 function moveRight(){
 	$("#img-place").fadeOut(1000, function(){
@@ -22,6 +27,18 @@ function moveRight(){
 			index = 0;
 		}
 		image[0].setAttribute('src', "images/" + images[index] + "-rose.jpg");
+
+		if (image[0].height > image[0].width){
+			console.log('y');
+			$('#img-place').css('margin-top', 0);
+			$("#img-place").css('height', 555);
+		}
+		else{
+			console.log('n');
+			$('#img-place').css('padding-top', 60);
+			// $('#img-place').css('padding-bottom', -60);
+			$("#img-place").css('height', 570);
+		}
 	}); 
 	$("#img-place").fadeIn(2000);
 }
@@ -53,7 +70,12 @@ function startTimer(){
 // 	//document.addEventListener('click', startTimer);
 // }
 
+//window.addEventListener('load',resize);
 //window.addEventListener('load', startTimer);
 
+//leftButton.addEventListener('click',resize);
 leftButton.addEventListener('click', moveLeft);
+
+//rightButton.addEventListener('click', resize);
 rightButton.addEventListener('click', moveRight);
+
